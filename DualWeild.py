728cc72f59fb4e304cc4ca6c66426c06f0199a9d
Crisgol98-PC <crisgol98@gmail.com>
2024-04-29 19:37:05 +0200

    0.2

--- DualWeild.py
+++ DualWeild.py
@@ -9,23 +9,33 @@ mouse = MouseController()
 keyboard = KeyboardController()
 # Constantes
 click_delay = 0.04
-
 macro_active = False
 senzu_active = False
 
 
 def on_click(x, y, button, pressed):
     global macro_active
+    if win32gui.GetWindowText(win32gui.GetForegroundWindow()) != "Minecraft 1.7.10":
+        return
+    # Dual Weild
     if button == Button.x2 and pressed:
         macro_active = not macro_active
         if macro_active:
             threading.Thread(target=dual_weild).start()
+    # Eat Senzu
     elif button == Button.x1 and pressed:
-        if not macro_active and not senzu_active:
-            threading.Thread(target=eat_senzu)
+        macro_changed = False
+        if macro_active:
+            macro_active = False
+            macro_changed = True
+        if not senzu_active:
+            threading.Thread(target=eat_senzu).start()
+        if macro_changed:
+            macro_active = True
 
 
 def dual_weild():
+    global macro_active
     while macro_active:
         mouse.click(Button.left)
         time.sleep(click_delay)
@@ -42,16 +52,10 @@ def dual_weild():
 def eat_senzu():
     global macro_active, senzu_active
     senzu_active = True
-    macro_changed = False
-    if macro_active:
-        macro_active = False
-        macro_changed = True
     keyboard.press('9')
     keyboard.release('9')
     time.sleep(click_delay)
     mouse.click(Button.right)
-    if macro_changed:
-        macro_active = True
     senzu_active = False
 
 
